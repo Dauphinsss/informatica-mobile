ce0d698a3123227f3db3911e202e7b18d24159fe
SteveCasTo <steven18122004@gmail.com>
2025-10-29 00:50:09 -0400

    fix: smoother post viewer integration in "Mis Publicaciones" section

--- app/(tabs)/ProfileStack.tsx
+++ app/(tabs)/ProfileStack.tsx
@@ -2,14 +2,31 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import MisPublicacionesScreen from "../profile/MisPublicacionesScreen";
 import ProfileScreen from "./profile";
+import PublicationDetailScreen from "../subjects/PublicationDetailScreen";
+import FileGalleryScreen from "../subjects/FileGalleryScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function ProfileStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="ProfileMain" component={ProfileScreen} />
-      <Stack.Screen name="MisPublicacionesScreen" component={MisPublicacionesScreen} />
+      <Stack.Screen
+        name="ProfileMain"
+        component={ProfileScreen}
+      />
+      <Stack.Screen
+        name="MisPublicacionesScreen"
+        component={MisPublicacionesScreen}
+      />
+      <Stack.Screen
+        name="PublicationDetail"
+        component={PublicationDetailScreen}
+      />
+      <Stack.Screen
+        name="FileGallery"
+        component={FileGalleryScreen}
+        options={{ headerShown: false, animation: 'slide_from_bottom' }}
+      />
     </Stack.Navigator>
   );
 }
